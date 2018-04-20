99f983611efdee0665c95831a1bce859ba36f66f
Kevin <kevin07liu@gmail.com>
2018-04-20 16:47:27 +0600

    feat: Add Router 'react-native-router-flux' to add Navigation funcionality for the app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import ReduxThunk from 'redux-thunk';
 
 import firebaseconfig from './firebase_config.json';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+
+import Router from './Router';
+
 
 class App extends Component {
   componentWillMount() {
@@ -16,7 +18,7 @@ class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
