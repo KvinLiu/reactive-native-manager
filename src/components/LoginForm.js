1234573d68a9bd4693dce0226db6630a8dabcd22
Kevin <kevin07liu@gmail.com>
2018-04-20 16:30:08 +0600

    feat: Add Spinner to the app to handle user login state. now user login functionality is done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 
 class LoginForm extends Component {
@@ -27,6 +27,18 @@ class LoginForm extends Component {
         </View>
       )
     }
+  }
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large"/>
+    }
+    return (
+      <Button
+        onPress={this.onButtonPress.bind(this)}
+        >
+        Login
+      </Button>
+    )
   }
 
   render() {
@@ -55,11 +67,7 @@ class LoginForm extends Component {
         {this.renderError()}
 
         <CardSection>
-          <Button
-            onPress={this.onButtonPress.bind(this)}
-            >
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -75,11 +83,12 @@ const styles = {
 }
 
 const mapStateToProps = state => {
-  const { email, password, error } = state.auth;
+  const { email, password, error, loading } = state.auth;
   return {
     email,
     password,
-    error
+    error,
+    loading
   }
 }
 
