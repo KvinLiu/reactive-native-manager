781a5213c12147bc0f665b2674db07881482221e
Kevin <kevin07liu@gmail.com>
2018-04-21 21:18:47 +0600

    feat: Add functionality to fetch employees form firebase.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
+
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
   render() {
     return (
       <View>
@@ -12,8 +18,12 @@ class EmployeeList extends Component {
         <Text>Employee List</Text>
         <Text>Employee List</Text>
       </View>
-    )
+    );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+
+};
+
+export default connect(null, { employeesFetch })(EmployeeList);
